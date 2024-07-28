dcd5a4da24167e421b56250ad59721af3b1e4b9b
Guilherme Chehade <gui_chehade@hotmail.com>
2024-07-28 18:12:46 -0300

    Implementação de modo light e dark, ajustes nos inputs e menu hamburguer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/header.css';
 
-const Header = ({ toggleTheme }) => (
-  <header className="header">
-    <div className="logo">🐶</div>
-    <nav className="nav">
-      <ul className="ul">
-        <li className="li"><Link className="link" to="/">Início</Link></li>
-        <li className="li"><Link className="link" to="/cadastro">Cadastro</Link></li>
-        <li className="li"><Link className="link" to="/controle">Controle</Link></li>
-        <li className="li"><Link className="link" to="/financas">Finanças</Link></li>
-      </ul>
-    </nav>
-    <button className="themeToggle" onClick={toggleTheme}>🌗</button>
-  </header>
-);
+const Header = ({ toggleTheme }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">🐶</div>
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
+        <ul className="ul">
+          <li className="li"><Link className="link" to="/">Início</Link></li>
+          <li className="li"><Link className="link" to="/cadastro">Cadastro</Link></li>
+          <li className="li"><Link className="link" to="/controle">Controle</Link></li>
+          <li className="li"><Link className="link" to="/financas">Finanças</Link></li>
+        </ul>
+      </nav>
+      <button className="themeToggle" onClick={toggleTheme}>🌗</button>
+      <button className="hamburger" onClick={toggleMenu}>☰</button>
+    </header>
+  );
+};
 
 export default Header;
