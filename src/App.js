b26f9b03ed39a28770c94c1e5b2886a4aa5513f9
Guilherme Chehade <gui_chehade@hotmail.com>
2024-07-29 14:44:26 -0300

    header

--- src/App.js
+++ src/App.js
@@ -53,8 +53,8 @@ function App() {
             <Route path="/cadastro" element={isAuthenticated ? <Cadastro /> : <Navigate to="/login" />} />
             <Route path="/controle" element={isAuthenticated ? <Controle /> : <Navigate to="/login" />} />
             <Route path="/financas" element={isAuthenticated && isAdmin ? <Financas /> : <Navigate to="/login" />} />
-            <Route path="/editar-pet/:id" element={isAuthenticated && isAdmin ? <EditarPet /> : <Navigate to="/login" />} />
-            <Route path="/creche" element={isAuthenticated && isAdmin ? <Creche /> : <Navigate to="/login" />} />
+            <Route path="/editar-pet/:id" element={isAuthenticated ? <EditarPet /> : <Navigate to="/login" />} />
+            <Route path="/creche" element={isAuthenticated ? <Creche /> : <Navigate to="/login" />} />
             <Route path="/usuarios" element={isAuthenticated ? <Usuarios /> : <Navigate to="/login" />} />
           </Routes>
         </div>
