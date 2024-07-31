66f71eb8631b4df39fbe6c81e4ebe949a550bef0
Guilherme Chehade <gui_chehade@hotmail.com>
2024-07-31 11:56:40 -0300

    Ajustar CSS do Header e adicionar menu responsivo

--- src/App.js
+++ src/App.js
@@ -17,15 +17,17 @@ function App() {
   return (
     <Router>
       <Header isAuthenticated={isAuthenticated} userRoles={userRoles} />
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} setUserRoles={setUserRoles} />} />
-        <Route path="/creche" element={isAuthenticated ? <Creche /> : <Navigate to="/login" />} />
-        <Route path="/financas" element={isAuthenticated && userRoles.isOwner ? <Financas /> : <Navigate to="/login" />} />
-        <Route path="/usuarios" element={isAuthenticated && userRoles.isOwner ? <Usuarios /> : <Navigate to="/login" />} />
-        <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
-      </Routes>
+      <div style={{ paddingTop: '60px' }}>
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/register" />} />
+          <Route path="/cadastro" element={isAuthenticated ? <Cadastro /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} setUserRoles={setUserRoles} />} />
+          <Route path="/creche" element={isAuthenticated ? <Creche /> : <Navigate to="/login" />} />
+          <Route path="/financas" element={isAuthenticated && userRoles.isOwner ? <Financas /> : <Navigate to="/login" />} />
+          <Route path="/usuarios" element={isAuthenticated && userRoles.isOwner ? <Usuarios /> : <Navigate to="/login" />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/header.css';
-import logoLarge from '../assets/logo-large.png'; // Adicione sua imagem para telas grandes
-import logoSmall from '../assets/logo-small.png'; // Adicione sua imagem para telas pequenas
+import logoLarge from '../assets/logo-large.png';
+import logoSmall from '../assets/logo-small.png';
 
-const Header = ({ isAuthenticated, userRoles }) => (
-  <header className="header">
-    <div className="logo">
-      <img src={window.innerWidth > 768 ? logoLarge : logoSmall} alt="Logo" />
-    </div>
-    <nav className="nav">
-      <ul className="ul">
-        <li className="li"><Link className="link" to="/">Início</Link></li>
-        {isAuthenticated && (
-          <>
-            {(userRoles.isAdmin || userRoles.isOwner) && <li className="li"><Link className="link" to="/cadastro">Cadastro</Link></li>}
-            {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/contrato">Contrato</Link></li>}
-            {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/creche">Creche</Link></li>}
-            {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/hotel">Hotel</Link></li>}
-            {userRoles.isOwner && <li className="li"><Link className="link" to="/financas">Finanças</Link></li>}
-            {userRoles.isOwner && <li className="li"><Link className="link" to="/usuarios">Usuários</Link></li>}
-          </>
-        )}
-        {!isAuthenticated && <li className="li"><Link className="link" to="/login">Login</Link></li>}
-      </ul>
-    </nav>
-  </header>
-);
+const Header = ({ isAuthenticated, userRoles }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={window.innerWidth > 768 ? logoLarge : logoSmall} alt="Logo" />
+      </div>
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
+        <ul className="ul">
+          <li className="li"><Link className="link" to="/">Início</Link></li>
+          {isAuthenticated && (
+            <>
+              {(userRoles.isAdmin || userRoles.isOwner) && <li className="li"><Link className="link" to="/cadastro">Cadastro</Link></li>}
+              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/contrato">Contrato</Link></li>}
+              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/creche">Creche</Link></li>}
+              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/hotel">Hotel</Link></li>}
+              {userRoles.isOwner && <li className="li"><Link className="link" to="/financas">Finanças</Link></li>}
+              {userRoles.isOwner && <li className="li"><Link className="link" to="/usuarios">Usuários</Link></li>}
+            </>
+          )}
+          {!isAuthenticated && <li className="li"><Link className="link" to="/login">Login</Link></li>}
+        </ul>
+        <button className="hamburger" onClick={toggleMenu}>☰</button>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
