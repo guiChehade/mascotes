aaf92c603bfc80978a7b3d69897d9332265884bc
Guilherme Chehade <gui_chehade@hotmail.com>
2024-07-31 23:52:57 -0300

    Melhorias no layout do menu, cards e responsividade

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,14 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import '../styles/header.css';
 import logoLarge from '../assets/logo-large.png';
 import logoSmall from '../assets/logo-small.png';
 
-const Header = ({ isAuthenticated, userRoles }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  return (
-    <header className="header">
-      <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-        ☰
-      </button>
-      <div className="logo">
-        <img src={window.innerWidth > 768 ? logoLarge : logoSmall} alt="Logo" />
-      </div>
-      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
-        <ul className="ul">
-          <li className="li"><Link className="link" to="/">Início</Link></li>
-          {isAuthenticated && (
-            <>
-              {(userRoles.isAdmin || userRoles.isOwner) && <li className="li"><Link className="link" to="/cadastro">Cadastro</Link></li>}
-              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/contrato">Contrato</Link></li>}
-              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/creche">Creche</Link></li>}
-              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/hotel">Hotel</Link></li>}
-              {userRoles.isOwner && <li className="li"><Link className="link" to="/financas">Finanças</Link></li>}
-              {userRoles.isOwner && <li className="li"><Link className="link" to="/usuarios">Usuários</Link></li>}
-            </>
-          )}
-          {!isAuthenticated && <li className="li"><Link className="link" to="/login">Login</Link></li>}
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="header">
+    <div className="logo">
+      <img src={window.innerWidth > 768 ? logoLarge : logoSmall} alt="Logo" />
+    </div>
+  </header>
+);
 
 export default Header;
---
+++ src/components/Menu.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/menu.css';
+
+const Menu = ({ isAuthenticated, userRoles }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <>
+      <button className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={handleMenuClick}>
+        ☰
+      </button>
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
+        <ul className="ul">
+          <li className="li"><Link className="link" to="/" onClick={handleLinkClick}>Início</Link></li>
+          {isAuthenticated && (
+            <>
+              {(userRoles.isAdmin || userRoles.isOwner) && <li className="li"><Link className="link" to="/cadastro" onClick={handleLinkClick}>Cadastro</Link></li>}
+              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/contrato" onClick={handleLinkClick}>Contrato</Link></li>}
+              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/creche" onClick={handleLinkClick}>Creche</Link></li>}
+              {(userRoles.isEmployee || userRoles.isOwner) && <li className="li"><Link className="link" to="/hotel" onClick={handleLinkClick}>Hotel</Link></li>}
+              {userRoles.isOwner && <li className="li"><Link className="link" to="/financas" onClick={handleLinkClick}>Finanças</Link></li>}
+              {userRoles.isOwner && <li className="li"><Link className="link" to="/usuarios" onClick={handleLinkClick}>Usuários</Link></li>}
+            </>
+          )}
+          {!isAuthenticated && <li className="li"><Link className="link" to="/login" onClick={handleLinkClick}>Login</Link></li>}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Menu;
