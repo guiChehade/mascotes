9ebdd7450eca05d5c97bf1234026f56a656d1f54
Guilherme Chehade <gui_chehade@hotmail.com>
2024-08-01 18:48:25 -0300

    correção de menu hamburguer

--- src/pages/Creche.js
+++ src/pages/Creche.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import { firestore } from '../firebase';
 import { useNavigate } from 'react-router-dom';
-import CrecheCard from '../components/CrecheCard';
 import '../styles/creche.css';
+import CrecheCard from '../components/CrecheCard';
 
 const Creche = () => {
   const [pets, setPets] = useState([]);
@@ -10,18 +11,10 @@ const Creche = () => {
 
   useEffect(() => {
     const fetchPets = async () => {
-      try {
-        const petsSnapshot = await firestore.collection('pets').get();
-        const petsData = petsSnapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
-        setPets(petsData);
-      } catch (error) {
-        console.error("Erro ao buscar pets:", error);
-      }
+      const querySnapshot = await getDocs(collection(firestore, 'pets'));
+      const petsList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setPets(petsList);
     };
-
     fetchPets();
   }, []);
 
